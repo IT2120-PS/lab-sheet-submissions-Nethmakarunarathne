7aeb75f0744f7211b5cc48ef4968cacd5f2e37ea
IT24100528 <IT24100528@my.sliit.lk>
2025-09-26 09:56:24 +0530

    Add files via upload

---
+++ Lab Sheet 07/LAB_7_ps.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\NethmaKarunarathne\\OneDrive\\Desktop\\IT24100528_LAB7")
+
+#Exercise
+#1)
+punif(25,min=0,max=40,lower.tail = TRUE) - punif(10,min=0,max=40,lower.tail = TRUE)
+
+#2)
+pexp(2, rate = (1/3), lower.tail = TRUE)
+
+#3)
+#i)
+1 - pnorm(130, mean = 100, sd = 15, lower.tail = TRUE)
+
+#ii)
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
+
+
